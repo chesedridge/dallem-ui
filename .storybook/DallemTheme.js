87419860900278979fa1b94a19dd7f302c0078ed
Dimi Lee <bash.ruv@kakao.com>
2022-01-18 18:04:54 +0900

    docs: publish on vercel

--- .storybook/DallemTheme.js
+++ .storybook/DallemTheme.js
@@ -2,36 +2,8 @@ import { create } from "@storybook/theming";
 
 export default create({
   base: "dark",
-
-  // colorPrimary: "hotpink",
   colorSecondary: "#F1C2B9",
-
-  // // UI
-  // appBg: "white",
-  // appContentBg: "silver",
-  // appBorderColor: "grey",
-  // appBorderRadius: 4,
-
-  // // Typography
-  // fontBase: '"Open Sans", sans-serif',
-  // fontCode: "monospace",
-
-  // // Text colors
-  // textColor: "black",
-  // textInverseColor: "rgba(255,255,255,0.9)",
-
-  // // Toolbar default and active colors
-  // barTextColor: "silver",
-  // barSelectedColor: "black",
-  // barBg: "hotpink",
-
-  // // Form colors
-  // inputBg: "white",
-  // inputBorder: "silver",
-  // inputTextColor: "black",
-  // inputBorderRadius: 4,
-
-  brandTitle: "My custom storybook",
-  brandUrl: "https://example.com",
-  brandImage: "/dallem-logo.png",
+  brandTitle: "달램 스토리북",
+  brandUrl: "https://ui.chesedridge.com",
+  brandImage: ".png",
 });
